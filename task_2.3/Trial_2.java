633bf6b16c1a965511bfc47c68d19ed1b3435f6a
Alexey <82582171+unofear@users.noreply.github.com>
2021-04-22 17:30:54 +0300

    Update Trial_2.java

--- task_2.3/Trial_2.java
+++ task_2.3/Trial_2.java
@@ -1,1 +1,11 @@
-
+public class Trial_2{
+    public static void main(String[] args){
+        String a = ("8");
+        for (int i=1; i<=10; i++) {
+            for (int j = 1; j<i; j++) {
+                System.out.print(a);
+            }
+                System.out.println();
+        }
+    }
+}
