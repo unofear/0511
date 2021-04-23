163c4c2d811e2c64a4b74299bfbb8630de7aae32
Alexey <82582171+unofear@users.noreply.github.com>
2021-04-23 17:18:40 +0300

    Update Trial.java

--- task_1.5/Trial.java
+++ task_1.5/Trial.java
@@ -3,7 +3,7 @@ public class Trial_2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите температуру в цельсиях: ");
-        int TC = scan.nextInt();
+        double TC = scan.nextDouble();
         double TF = (9./5)*TC+32;
         System.out.print("Температура в фаренгейтах: ");
         System.out.println(TF);
