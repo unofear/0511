75509dcd43b77774b956ae9cf5eea3558af7c8d2
Alexey <82582171+unofear@users.noreply.github.com>
2021-04-23 17:10:48 +0300

    Update Trial.java

--- task_1.1/Trial.java
+++ task_1.1/Trial.java
@@ -1,8 +1,8 @@
 public class Trial {
     public  static void main(String[] args) {
-           String firstname = "Alexey";
-           int age =30;
-           String city ="Moscow";
-           System.out.println("Hello! My name is "+firstname+". I am "+age+" years old. I am from "+city+" ");
+        String firstname = "Alexey";
+        int age =30;
+        String city ="Moscow";
+        System.out.println("Hello! My name is "+firstname+". I am "+age+" years old. I am from "+city+".");
     }
 }
