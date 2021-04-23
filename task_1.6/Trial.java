dcd46488c5c91880e8a19d3f0d807f287de792f4
Alexey <82582171+unofear@users.noreply.github.com>
2021-04-23 17:20:57 +0300

    Update Trial.java

--- task_1.6/Trial.java
+++ task_1.6/Trial.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-public class Trial {
+public class Trial_2 {
     public static void main(String[] args) {
         Scanner ent = new Scanner(System.in);
-        int a = ent.nextInt();
+        double a = ent.nextInt();
         System.out.println(a+a/100*15);
     }
 }
