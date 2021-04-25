50061a904143b547f103e61cf7cbbb4f07c4c213
Alexey <82582171+unofear@users.noreply.github.com>
2021-04-26 01:54:50 +0300

    Update Trial_2.java

--- task_2.4/Trial_2.java
+++ task_2.4/Trial_2.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class Trial_2 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner ent = new Scanner(System.in);
-        System.out.print("Введите сумму вклада: ");
-        double contribution = ent.nextDouble();
-        System.out.print("Введите процент по вкладу: ");
-        double percent = ent.nextDouble();
-        for (int j=0; j<=5; j++);
-            System.out.println("Ваш доход :"+(contribution+(contribution*percent)/100));
+            System.out.print("Введите сумму вклада: ");
+        int contribution = ent.nextInt();
+            System.out.print("Введите процент по вкладу: ");
+        int percent = ent.nextInt();
+            for (int i=1; i<=5; i++) {
+                contribution += (contribution*percent)/100;
+        System.out.println("Ваш доход: "+contribution);}
     }
 }
