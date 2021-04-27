e1014b8308ab6502aa3f9f64ab8746cd95194548
Alexey <82582171+unofear@users.noreply.github.com>
2021-04-28 01:32:32 +0300

    Create Trial_2.java

---
+++ task_2.5/Trial_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Trial_2 {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        int a = ent.nextInt();{
+            if (a==10) {
+                System.out.println("Верно.");
+            }else{
+                System.out.println("Неверно.");
+            }
+        }
+    }
+}
