3890f5144a53b6990aaf840944165b1058390943
Alexey <82582171+unofear@users.noreply.github.com>
2021-04-29 20:49:24 +0300

    Update Trial.java

--- task_1.3/Trial.java
+++ task_1.3/Trial.java
@@ -2,15 +2,17 @@ public class Trial {
     public static void main(String[] args) {
         int a,b;
         a = 15;
-        b = 5;
+        b = 15;
         int sum = a+b;
         int sub = a-b;
         int mult = a*b;
         int div = a/b;
+        int sqr = (mult * mult);
         String s = "Результат вычисления";
         System.out.println(s+" "+"суммы: " +sum);
-        System.out.println(s+" "+"разности: "+sub);
+        System.out.println(s+" "+"разности: " +sub);
         System.out.println(s+" "+"произведения: " +mult);
         System.out.println(s+" "+"частного: " +div);
+        System.out.println(s+" "+"возведения в квадрат: " +sqr);
     }
 }
