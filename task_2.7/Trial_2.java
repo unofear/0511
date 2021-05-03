ac58aa44a1a968bdc31077d1676f6c0dc5d37510
Alexey <82582171+unofear@users.noreply.github.com>
2021-05-03 19:50:14 +0300

    Update Trial_2.java

--- task_2.7/Trial_2.java
+++ task_2.7/Trial_2.java
@@ -4,28 +4,12 @@ public class Trial_2 {
         Scanner ent = new Scanner(System.in);
         System.out.print("Введите переменную : ");
         int a = ent.nextInt();
-            for (int i = 2; i < a; i++)
-                if ( a%i==0) {
-                    System.out.println(false);
-                }
-                else{
-                    System.out.println(true);
+        for (int i = 2; i < a; i++)
+            if ( a%i!=1) {
+                System.out.println(false);
             }
-    }
-}
-
-import java.util.Scanner;
-class Trial_2 {
-    public static void main(String[] args) {
-        Scanner ent = new Scanner(System.in);
-        System.out.print("Введите переменную : ");
-        int a = ent.nextInt();
-            for (int i = 2; i < a; i++)
-                if ( a%i!=1) {
-                    System.out.println(false);
-                }
-                else{
-                    System.out.println(true);
+            else{
+                System.out.println(true);
             }
     }
 }
