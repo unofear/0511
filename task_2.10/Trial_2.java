8b02d1e4e9700d63b0497cc0391b29640d3e0c9a
Alexey <82582171+unofear@users.noreply.github.com>
2021-05-10 14:24:12 +0300

    Update Trial_2.java

--- task_2.10/Trial_2.java
+++ task_2.10/Trial_2.java
@@ -1,25 +1,12 @@
 public class Trial_2 {
     public static void main(String[] args) {
-        int[] mass = {1,2,3,4,4,5,6,7,7,8,9,10};//i=0...10
-        for (int i = 0; mass.length > i; i++) {
-            if (mass[i + 1] != mass[i])
-                System.out.println("Нет");
-            else
+        int[] mass = {1,2,3,4,4,5,6,7,7,8,9,1};//i=0...9
+        for (int i = 0; (i <= mass.length); i++) {
+            if (mass[i] == mass[i + 1]) {
                 System.out.println("Да");
+            } else {
+                System.out.println("Нет");
+            }
         }
     }
 }
-"C:\Program Files\Java\jdk-11.0.10\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2021.1\lib\idea_rt.jar=54036:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2021.1\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\Лёлик\IdeaProjects\Trials\out\production\Trials Trial_2
-        Нет
-        Нет
-        Нет
-        Да
-        Нет
-        Нет
-        Нет
-        Да
-        Нет
-        Нет
-        Нет
-        Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 12 out of bounds for length 12
-        at Trial_2.main(Trial_2.java:5)
